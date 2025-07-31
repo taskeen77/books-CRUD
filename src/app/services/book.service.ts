814b5a27663dfc648f458b4562299c07c04d8bec
taskeen77 <taskeensadiq49@gmail.com>
2025-08-01 01:17:42 +0500

    Add fallback using books.json and updated BookService

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,33 +1,75 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Book } from '../models/book.model';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class BookService {
+//   private apiUrl = 'http://localhost:3000/books';
+
+//   constructor(private http: HttpClient) {}
+
+//   getBooks(): Observable<Book[]> {
+//     return this.http.get<Book[]>(this.apiUrl);
+//   }
+
+//   addBook(book: Book): Observable<Book> {
+//     return this.http.post<Book>(this.apiUrl, book);
+//   }
+
+//   updateBook(id: number, book: Book): Observable<Book> {
+//     return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+//   }
+
+//   deleteBook(id: number): Observable<any> {
+//     return this.http.delete(`${this.apiUrl}/${id}`);
+//   }
+
+//   getBookById(id: number): Observable<Book> {
+//     return this.http.get<Book>(`${this.apiUrl}/${id}`);
+//   }
+// }
+
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Book } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private apiUrl = 'http://localhost:3000/books';
+  private liveApiUrl = 'https://taskeensadiq49--3000.local.webcontainer.io/books';
+  private mockUrl = 'assets/books.json'; 
 
   constructor(private http: HttpClient) {}
 
+
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiUrl);
+    return this.http.get<Book[]>(this.liveApiUrl).pipe(
+      catchError(error => {
+        console.warn('Replit API unreachable. Loading mock data instead.', error);
+        return this.http.get<Book[]>(this.mockUrl);
+      })
+    );
   }
 
   addBook(book: Book): Observable<Book> {
-    return this.http.post<Book>(this.apiUrl, book);
+    return this.http.post<Book>(this.liveApiUrl, book);
   }
 
   updateBook(id: number, book: Book): Observable<Book> {
-    return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+    return this.http.put<Book>(`${this.liveApiUrl}/${id}`, book);
   }
 
   deleteBook(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.liveApiUrl}/${id}`);
   }
 
   getBookById(id: number): Observable<Book> {
-    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+    return this.http.get<Book>(`${this.liveApiUrl}/${id}`);
   }
 }
